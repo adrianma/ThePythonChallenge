d311ee7d4f492ce2dd2e43a11b5323526eba9816
adrianma <adrianma@users.noreply.github.com>
2014-06-18 14:33:37 +0200

    Final solution
    Most common character is x

--- python_challenge_2.py
+++ python_challenge_2.py
@@ -6,9 +6,14 @@ Created on Mon Jun 16 23:13:40 2014
 """
 
 import urllib
+from collections import Counter
 
-sock = urllib.urlopen("http://www.pythonchallenge.com/pc/def/ocr.html")
-htmlSource = sock.read(); 
-sock.close();
-print htmlSource
+# Read the HTML source and store it
+oSock = urllib.urlopen("http://www.pythonchallenge.com/pc/def/ocr.html")
+tHtmlSource = oSock.read(); 
+oSock.close();
+
+# Find the most common character within the HTML source and print it out
+vCharacterFreq = Counter(tHtmlSource.strip())
+print(vCharacterFreq .most_common()[-1])
 
